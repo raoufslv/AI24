2eaebd79293cab582ec561701984575f0b5412a0
raoufslv <raoufslv09@gmail.com>
2024-03-21 12:22:06 +0100

    fixing the lazy load height on the add banner

--- src/components/shared/BigBanner.jsx
+++ src/components/shared/BigBanner.jsx
@@ -4,17 +4,17 @@ import LazyLoad from "react-lazy-load";
 
 export default function BigBanner() {
   // Calculate the appropriate height based on the screen width
-  const screenHeight = window.innerHeight;
-  let imageHeight = screenHeight * 0.03; // Set the height to 10% of the screen height initially
+  const screenWidth = window.innerWidth;
+  let imageWidth = screenWidth * 0.03; // Set the Width to 10% of the screen Width initially
 
-  // Adjust the height based on screen width
+  // Adjust the Width based on screen width
   if (window.innerWidth >= 1024) {
     // For large screens and above
-    imageHeight = screenHeight * 0.1; // Set the height to 20% of the screen height
+    imageWidth = screenWidth * 0.1; // Set the Width to 20% of the screen Width
   }
 
   return (
-    <LazyLoad height={imageHeight}>
+    <LazyLoad Width={imageWidth}>
       <img
         className="w-full mx-auto xl:h-32 rounded xl:object-fill object-scale-down object-center"
         src={TempBanner}
