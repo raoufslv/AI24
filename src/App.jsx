d340f5e281a39137d06693e2fc41cc52c65a6e71
Raouf <raoufslv09@gmail.com>
2024-03-24 18:41:45 +0100

    finishing the contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "./context/theme-provider";
 import { Fragment } from "react";
 import Router from "./routes/Router";
+import { Toaster } from "@/components/ui/toaster"
 
 function App() {
   return (
     <ThemeProvider>
       <Fragment>
         <Router />
+        <Toaster />
       </Fragment>
     </ThemeProvider>
   );
--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,10 +1,11 @@
+import { useRef } from "react";
+
 import SubmitButton from "@/components/customUI/forms/SubmitButton";
 import FormInput from "@/components/customUI/forms/FormInput";
 import FormTextarea from "@/components/customUI/forms/FormTextarea";
 import ErrorMessage from "@/components/customUI/forms/ErrorMessage";
 import emailjs from "@emailjs/browser";
-import { configDotenv } from "dotenv";
-configDotenv();
+import { useToast } from "@/components/ui/use-toast";
 
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -18,11 +19,14 @@ const ContactFormSchema = z.object({
 });
 
 export default function ContactForm() {
+  const formRef = useRef(null);
+  const { toast } = useToast();
   const {
     register,
     handleSubmit,
     setError,
     formState: { errors, isSubmitting },
+    setValue,
   } = useForm({
     defaultValues: {
       name: "",
@@ -35,13 +39,29 @@ export default function ContactForm() {
 
   const onSubmit = async (data) => {
     try {
-      console.log("data : ", data);
-      await emailjs.send(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+      const response = await emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         data,
-        process.env.REACT_APP_EMAILJS_USER_ID
+        import.meta.env.VITE_EMAILJS_USER_ID
       );
+
+      if (response.status !== 200) {
+        throw new Error("Failed to send message. Please try again later");
+      }
+
+      // Show success message
+      toast({
+        variant: "success",
+        title: "Message sent successfully",
+        description: "We will get back to you soon",
+      });
+
+      // Clear the data object
+      setValue("name", "");
+      setValue("number", "");
+      setValue("email", "");
+      setValue("subject", "");
     } catch (error) {
       setError("root", {
         type: "manual",
@@ -52,6 +72,7 @@ export default function ContactForm() {
 
   return (
     <form
+      ref={formRef}
       action=""
       onSubmit={handleSubmit(onSubmit)}
       className="flex flex-col gap-4 bg-white dark:bg-black p-8 rounded-xl shadow-input w-full md:w-96"
