b1fe87fd25d269547bfd3de47f7a4acf8b117a9d
raoufslv <raoufslv09@gmail.com>
2024-04-08 14:04:35 +0100

    fixed .env typos

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -2,7 +2,7 @@ import { getAccessToken } from "@/context/accessToken";
 export let axiosInstance = axios.create();
 
 import axios from "axios";
-const BASE_URL = import.meta.env.VITE_PRODUCTIONURL;
+const BASE_URL = import.meta.env.VITE_PRODUCTIO_URL;
 
 export const createAxiosInstance = () => {
   axiosInstance = axios.create({
