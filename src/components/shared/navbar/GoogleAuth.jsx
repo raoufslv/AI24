e21d7c84cd3f78fe2e5b5f54fd6b0c7c43db8e76
raoufslv <raoufslv09@gmail.com>
2024-04-08 15:38:19 +0100

    adding finile function to the oAtuh

--- src/components/shared/navbar/GoogleAuth.jsx
+++ src/components/shared/navbar/GoogleAuth.jsx
@@ -1,8 +1,13 @@
 import { GoogleLogin } from "@react-oauth/google";
 import { useGoogleLoginMutation } from "@/hooks/react-query/useAuth";
+import { setAccessToken } from "@/context/accessToken";
+import { useAuth } from "@/context/AuthContext";
+import { createAxiosInstance } from "@/services/apiConfig";
 
 export default function GoogleAuth() {
   const GoogleLoginMutation = useGoogleLoginMutation();
+  const { setConnected, setRole, setImage, setFirstName, setOpenLogin } =
+    useAuth();
   return (
     <GoogleLogin
       onSuccess={async (credentialResponse) => {
@@ -12,6 +17,14 @@ export default function GoogleAuth() {
           credential: credentialResponse.credential,
         });
         console.log(response);
+        setAccessToken(response.accessToken);
+        setConnected(true);
+        setRole(response.role);
+        setImage(response.image);
+        setFirstName(response.firstName);
+        createAxiosInstance();
+        // close the modal
+        setOpenLogin(false);
       }}
       onError={() => {
         console.log("Login Failed");
